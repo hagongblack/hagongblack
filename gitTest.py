e6aa4ad12f3b2a03c8cefdffafe40e069d7b4702
hagongblack <howeonyoo@gmail.com>
2022-11-08 13:33:49 +0900

    second commit

--- gitTest.py
+++ gitTest.py
@@ -3,6 +3,7 @@ import socket
 UDP_IP = "172.20.202.22"
 UDP_PORT = 65022
 MESSAGE = b"Hello, Im 172.20.1.68\n"
+MESSAGE1 = b"Hello, Im 172.20.1.69\n"
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
@@ -11,3 +12,11 @@ print("message: %s" % MESSAGE)
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE1)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE1, (UDP_IP, UDP_PORT))
