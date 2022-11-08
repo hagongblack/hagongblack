fe6cc228ced156895890350bfc220bb247ec1752
hagongblack <howeonyoo@gmail.com>
2022-11-08 13:52:45 +0900

    third commit

--- gitTest.py
+++ gitTest.py
@@ -9,14 +9,16 @@ print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 print("message: %s" % MESSAGE)
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 print("message: %s" % MESSAGE1)
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
 sock.sendto(MESSAGE1, (UDP_IP, UDP_PORT))
+
+print("test push")
